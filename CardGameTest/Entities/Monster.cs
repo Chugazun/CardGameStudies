9a1a5eb6bf7ec58e11ebc6750c5f773df87607e7
Samuel Silva <samuelrodolfos@gmail.com>
2020-01-28 13:20:01 -0300

    Initial Commit pre EFCore

---
+++ CardGameTest/Entities/Monster.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGameTest.Entities
+{
+    class Monster : Entity
+    {
+        public string Name { get; set; }
+
+        public Monster(string name, int hp) : base(hp)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ CardGameTest/Entities/Player.cs
@@ -0,0 +1,18 @@
+﻿using CardGameTest.Utils;
+using System.Collections.Generic;
+
+namespace CardGameTest.Entities
+{
+    class Player : Entity
+    {       
+        public int Gold { get; private set; }
+        public int Level { get; private set; }
+        public int Exp { get; set; }
+        public PlayerBag PlayerBag { get; set; } = new PlayerBag();
+
+        public Player(int hp) : base (hp)
+        {            
+            Level = 1;
+        }
+    }
+}
